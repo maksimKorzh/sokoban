d20ac25763e6580310ea1862d8c21137aff74632
Maksim Korzh <freesoft.for.people@gmail.com>
2019-03-23 00:53:58 +0200

    added 'sokoban-game.c' that uses 'game.h' console game library

---
+++ game.h
@@ -0,0 +1,164 @@
+/*********************************************************************************\
+;                                     game.h                                      ;
+;---------------------------------------------------------------------------------;
+;                    A very basic console game library dedicated                  ;
+;                for the beginners who want to write their own games              ;
+;---------------------------------------------------------------------------------;
+;                                by Maksim Korzh                                  ;
+;---------------------------------------------------------------------------------;
+\*********************************************************************************/
+
+/*********************************************************************************\
+;---------------------------------------------------------------------------------;
+;        Copyright © 2018 Maksim Korzh <freesoft.for.people@gmail.com>            ;
+;---------------------------------------------------------------------------------;
+;     This work is free. You can redistribute it and/or modify it under the       ;
+;      terms of the Do What The Fuck You Want To Public License, Version 2,       ;
+;       as published by Sam Hocevar. See the COPYING file for more details.       ;
+;---------------------------------------------------------------------------------;
+'       THIS PROGRAM IS FREE SOFTWARE. IT COMES WITHOUT ANY WARRANTY, TO          ;
+;        THE EXTENT PERMITTED BY APPLICABLE LAW. YOU CAN REDISTRIBUTE IT          ;
+;       AND/OR MODIFY IT UNDER THE TERMS OF THE DO WHAT THE FUCK YOU WANT         ;
+;          TO PUBLIC LICENCE, VERSION 2, AS PUBLISHED BY SAM HOCEVAR.             ;
+;                SEE http://www.wtfpl.net/ FOR MORE DETAILS.                      ;
+;---------------------------------------------------------------------------------;
+\*********************************************************************************/
+
+/*********************************************************************************\
+;---------------------------------------------------------------------------------;
+;                   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                   ;
+;                           Version 2, December 2004                              ;
+;                                                                                 ;
+;        Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>                         ;
+;                                                                                 ;
+;        Everyone is permitted to copy and distribute verbatim or modified        ;
+;        copies of this license document, and changing it is allowed as long      ;
+;        as the name is changed.                                                  ;
+;                                                                                 ;
+;                   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                   ;
+;          TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION        ;
+;                                                                                 ;
+;         0. You just DO WHAT THE FUCK YOU WANT TO.                               ;
+;---------------------------------------------------------------------------------;
+\*********************************************************************************/
+
+#include <stdio.h>
+
+// default console screen size
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+#define SCREEN_SIZE SCREEN_WIDTH * SCREEN_HEIGHT
+
+// screen buffer that replaces stdout
+char screen[SCREEN_SIZE];
+
+// init windows headers
+#ifdef WIN32
+
+#include <windows.h>
+#include <stdlib.h>
+HANDLE console;
+CONSOLE_CURSOR_INFO cursor;
+COORD coord;
+DWORD chars_to_write = 0;
+
+#endif
+
+// init unix headers
+#ifdef unix
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+#endif
+
+void InitScreen()
+{
+    // print directly to screen buffer
+    #ifdef WIN32
+    console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);    
+    cursor.dwSize = 100;
+    cursor.bVisible = FALSE;    
+    coord.X = 0;
+    coord.Y = 0;
+    SetConsoleActiveScreenBuffer(console);
+    SetConsoleCursorInfo(console, &cursor);        
+    #endif
+
+    // use ANSI VT100 escape sequences hide cursor and clear screen 
+    #ifdef unix
+    printf("\x1b[?25l");
+    printf("\x1b[2J");
+    #endif
+}
+
+void RefreshScreen()
+{
+    // might not be needed on windows natively, needed on wine though
+    for(int scr_cell = 0; scr_cell < SCREEN_SIZE; scr_cell++)
+        if(!screen[scr_cell]) screen[scr_cell] = ' ';
+
+    // update screen buffer
+    #ifdef WIN32
+    screen[SCREEN_SIZE - 1] = 0;
+	WriteConsoleOutputCharacter(console, screen, SCREEN_WIDTH * SCREEN_HEIGHT, coord, &chars_to_write);
+    #endif
+
+    // print screen buffer to stdout at coordinates 0, 0
+    #ifdef unix
+    printf("\x1b[0;0H%s\n", screen);
+    #endif
+}
+
+// getchar() for windows without echoing
+#ifdef WIN32
+
+int getch()
+{
+    DWORD mode, chars_to_read;
+    HANDLE console = GetStdHandle(STD_INPUT_HANDLE);
+        
+    GetConsoleMode(console, &mode);
+    SetConsoleMode(console, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
+    int key = 0;
+    ReadConsole(console, &key, 1, &chars_to_read, NULL);
+    SetConsoleMode(console, mode);
+    
+    return key;
+}
+
+#endif
+
+// getchar() for unix without echoing
+#ifdef unix
+
+int getch()
+{
+    struct termios oldattr, newattr;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    int key = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+
+    return key;   
+}
+
+#endif
+
+void Leave()
+{
+    #ifdef WIN32
+    cursor.bVisible = TRUE;
+    SetConsoleCursorInfo(console, &cursor);
+    #endif
+
+    // show cursor escape sequence
+    #ifdef unix
+    printf("\x1b[?25h");
+    #endif
+}
---
+++ sokoban-game.c
@@ -0,0 +1,207 @@
+
+/*********************************************************************************\
+;                                  SOKOBAN GAME                                   ;
+;---------------------------------------------------------------------------------;
+;                  Simple implementation of sokoban game dedicated                ;
+;                for the beginners who want to write their own games              ;
+;---------------------------------------------------------------------------------;
+;                                by Maksim Korzh                                  ;
+;---------------------------------------------------------------------------------;
+\*********************************************************************************/
+
+/*********************************************************************************\
+;---------------------------------------------------------------------------------;
+;        Copyright © 2018 Maksim Korzh <freesoft.for.people@gmail.com>            ;
+;---------------------------------------------------------------------------------;
+;     This work is free. You can redistribute it and/or modify it under the       ;
+;      terms of the Do What The Fuck You Want To Public License, Version 2,       ;
+;       as published by Sam Hocevar. See the COPYING file for more details.       ;
+;---------------------------------------------------------------------------------;
+'       THIS PROGRAM IS FREE SOFTWARE. IT COMES WITHOUT ANY WARRANTY, TO          ;
+;        THE EXTENT PERMITTED BY APPLICABLE LAW. YOU CAN REDISTRIBUTE IT          ;
+;       AND/OR MODIFY IT UNDER THE TERMS OF THE DO WHAT THE FUCK YOU WANT         ;
+;          TO PUBLIC LICENCE, VERSION 2, AS PUBLISHED BY SAM HOCEVAR.             ;
+;                SEE http://www.wtfpl.net/ FOR MORE DETAILS.                      ;
+;---------------------------------------------------------------------------------;
+\*********************************************************************************/
+
+/*********************************************************************************\
+;---------------------------------------------------------------------------------;
+;                   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                   ;
+;                           Version 2, December 2004                              ;
+;                                                                                 ;
+;        Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>                         ;
+;                                                                                 ;
+;        Everyone is permitted to copy and distribute verbatim or modified        ;
+;        copies of this license document, and changing it is allowed as long      ;
+;        as the name is changed.                                                  ;
+;                                                                                 ;
+;                   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                   ;
+;          TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION        ;
+;                                                                                 ;
+;         0. You just DO WHAT THE FUCK YOU WANT TO.                               ;
+;---------------------------------------------------------------------------------;
+\*********************************************************************************/
+
+#include "game.h"
+                
+#define MAP_WIDTH 8
+#define MAP_HEIGHT 10
+#define PLAYER_POSITION pos_y * MAP_WIDTH + pos_x
+
+char map[] = {
+
+    "#####   "
+    "#xB ### "
+    "###   # "
+    "#x@B  # "
+    "### Bx# "
+    "#x##B # "
+    "# # x ##"
+    "#B OBBx#"
+    "#   x  #"
+    "########"
+
+};
+
+int dest_squares[10];                                                   // array to store cell indexes for 'x' cells
+
+int GetDestSquares()                                                    // init 'x' cells indexes
+{
+    int count = 0, cell;                                                // 'x' cell number, current cell index
+
+    for(int row = 0; row < MAP_HEIGHT; row++)                           // loop ower map rows
+    {
+        for(int col = 0; col < MAP_WIDTH; col++)                        // loop ower map columns
+        {
+            cell = row * MAP_WIDTH + col;                               // init current cell index
+        
+            if(map[cell] == 'x' || map[cell] == 'O')                    // if 'x' cell is emty or with box on it
+                dest_squares[count++] = cell;                           // store it in array
+        }
+    }
+
+    return count;                                                       // return number of 'x' cells
+}
+
+void GetPosition(int *pos_x, int *pos_y)
+{
+    int cell;                                                           // current cell index
+
+    for(int row = 0; row < MAP_HEIGHT; row++)                           // loop ower map rows
+    {
+        for(int col = 0; col < MAP_WIDTH; col++)                        // loop ower map columns
+        {
+            cell = row * MAP_WIDTH + col;                               // init current cell index
+        
+            if(map[cell] == '@')                                        // if current cell on the map contains player
+            {
+                *pos_x = col;                                           // store player's x coordinate
+                *pos_y = row;                                           // store player's y coordinate
+            }
+        }
+    }
+}
+
+void MoveCharacter(int pos_x, int pos_y, int offset)
+{
+    if(map[PLAYER_POSITION + offset] != '#')                            // if player doesn't hit the wall
+    {
+        if(((map[PLAYER_POSITION + offset] == 'B') ||                   // if player hits the box
+            (map[PLAYER_POSITION + offset] == 'O')) &&                  // or the box on 'x' cell
+            (map[PLAYER_POSITION + offset * 2] != '#' ||                // and box doesn't hit a wall
+             map[PLAYER_POSITION + offset * 2] != 'B' ||                // or another box
+             map[PLAYER_POSITION + offset * 2] != 'O'))                 // or box on 'x' cell
+        {
+            map[PLAYER_POSITION] = ' ';                                 // clear previous player's position
+            pos_x += offset;                                            // update player's coordinate
+
+            if(map[PLAYER_POSITION + offset] == ' ')                    // if the square next to the box is empty
+                map[PLAYER_POSITION + offset] = 'B';                    // push the box
+
+            else if(map[PLAYER_POSITION + offset] == 'x')               // if the square next to the box is 'x'  
+                map[PLAYER_POSITION + offset] = 'O';                    // mark the box is on it's place
+
+            else
+            {
+                map[PLAYER_POSITION - offset] = '@';                    // if box hits the wall or another box
+                return;                                                 // don't push it any further
+            }
+
+            map[PLAYER_POSITION] = '@';                                 // draw the player in the new position
+        }
+
+        else                                                            // if the square next to the player is empty                                                        
+        {
+            map[PLAYER_POSITION] = ' ';                                 // clear previous player position
+            pos_x += offset;                                            // update player's coordinate
+            map[PLAYER_POSITION] = '@';                                 // draw the player in the new position
+        }
+    }   
+}
+
+void PrintMap(int pos_x, int pos_y, char *map)
+{
+    for (int row = 0; row < MAP_HEIGHT; row++)
+    {
+        for (int col = 0; col < MAP_WIDTH; col++)
+        {
+            screen[(row + pos_y) * SCREEN_WIDTH + col + pos_x] = map[row * MAP_WIDTH + col];
+        }
+    }
+
+    RefreshScreen();    
+}         
+
+int main()
+{
+    InitScreen();
+
+    int key;                                                            // user input key
+    int pos_x, pos_y;                                                   //  player's coordinates
+    int dest_count;                                                     //  'x' cells counter
+
+    int dest_num = GetDestSquares();
+
+    PrintMap(SCREEN_WIDTH / 2 - MAP_WIDTH / 2, SCREEN_HEIGHT / 2 - MAP_HEIGHT / 2, map);
+    
+    while(1)
+    {
+        if(key == 27) break;
+        
+        key = getch();
+        GetPosition(&pos_x, &pos_y);
+
+        switch(key)
+        {
+            case 'w': MoveCharacter(pos_x, pos_y, - MAP_WIDTH); break;
+            case 's': MoveCharacter(pos_x, pos_y, MAP_WIDTH); break;
+            case 'a': MoveCharacter(pos_x, pos_y, - 1); break;
+            case 'd': MoveCharacter(pos_x, pos_y, 1); break;
+        }
+
+        dest_count = 0;                                                 // reset 'x' cells counter
+
+        for(int i = 0; i < 10; i++)                                     // for all destination squares
+        {            
+            if(map[dest_squares[i]] == 'O') dest_count++;               // increase 'x' cells counter if box is on 'x' cell
+        
+            if(map[dest_squares[i]] == ' ')                             // if 'x' cell has been erased
+                map[dest_squares[i]] = 'x';                             // restore it
+        }
+        
+        PrintMap(SCREEN_WIDTH / 2 - MAP_WIDTH / 2, SCREEN_HEIGHT / 2 - MAP_HEIGHT / 2, map);
+
+        // if all boxes are on it's places break out of game loop
+        if(dest_num == dest_count)
+        {
+            sprintf(screen + (SCREEN_WIDTH * SCREEN_HEIGHT / 2) - MAP_WIDTH / 2, "YOU WIN!");
+            RefreshScreen();
+            break;
+        }       
+    }
+
+    Leave();
+    
+    return 0;
+}
